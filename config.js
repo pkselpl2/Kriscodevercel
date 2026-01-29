f24bd19dd38866fdc08a99dbc44a7f33f8f776b9
Teamkris <kkammailbox@gmail.com>
2026-01-29 12:43:47 +0545

    Add Firebase configuration to config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyClIIQUOrRwTXe1HVsijm1QL41oRymbzZs",
+  authDomain: "kriscodelogintest.firebaseapp.com",
+  projectId: "kriscodelogintest",
+  storageBucket: "kriscodelogintest.firebasestorage.app",
+  messagingSenderId: "612414399150",
+  appId: "1:612414399150:web:5266eab3ee40e04258833f",
+  measurementId: "G-QEM6ZVN527"
+};
